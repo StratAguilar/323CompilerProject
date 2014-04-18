1e631653c39af43ff9daee4de675748393da52f2
Stratton Aguilar <rabbitofalice@Stratton-Aguilars-MacBook-Pro.local>
2014-04-17 21:08:57 -0700

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Stratton Aguilar on 4/17/14.
+//  Copyright (c) 2014 Stratton Aguilar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
